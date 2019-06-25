dba795039641daa8813546079f7794dc990ba4b6
9731123 <erfanrmz@aut.ac.ir>
2019-06-25 12:22:07 +0430

    main panel Shit button deleted

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -5,8 +5,5 @@ public class MainPanel extends JPanel {
     public MainPanel()
     {
         this.setBackground(new Color(24,24,24));
-        JButton but = new JButton();
-//        but.setAction();
-        this.add(but);
     }
 }
