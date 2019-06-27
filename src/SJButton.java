8c711772760bf4472eb469a3638ad7dddecd01f5
9731123 <erfanrmz@aut.ac.ir>
2019-06-27 12:51:49 +0430

    playlist`s popup added to SJbutton

--- src/SJButton.java
+++ src/SJButton.java
@@ -6,19 +6,21 @@ import java.util.ArrayList;
 
 public class SJButton extends JButton {
     private Song song;
-    private  Play player;
+    private Play player;
     private Play[] player2;
     private int press;
     private MainFrame mainFrame;
     private ArrayList<Play> playingThreads;
+    private JPopupMenu popupMenu;
+    private JMenuItem addToPlayList;
     public Play getPlayer() {
         return player;
     }
 
-    public SJButton(String text, Icon icon, Song song , Play player, MainFrame mainFrame , ArrayList<Play> playingThreads) {
+    public SJButton(String text, Icon icon, Song song, Play player, MainFrame mainFrame, ArrayList<Play> playingThreads) {
         super(text, icon);
         this.song = song;
-        this.setSize(new Dimension(260,260));
+        this.setSize(new Dimension(260, 260));
         press = 0;
         this.mainFrame = mainFrame;
         this.setOpaque(false);
@@ -28,34 +30,41 @@ public class SJButton extends JButton {
         this.setVerticalTextPosition(SJButton.BOTTOM);
         this.setHorizontalTextPosition(JButton.CENTER);
         this.setForeground(Color.white);
-        this.setFont(new Font("",Font.BOLD,14));
+        this.setFont(new Font("", Font.BOLD, 14));
         this.player = player;
         player2 = new Play[2];
         this.playingThreads = playingThreads;
+        popupMenu = new JPopupMenu("Add to Playlist");
+        addToPlayList = new JMenuItem("Add To Playlist");
+        popupMenu.add(addToPlayList);
         this.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                try
-                {
-                    for (int i = 0 ; i < playingThreads.size() ; i++)
-                    {
-                     playingThreads.get(i).stop();
+                if (e.getButton() == MouseEvent.BUTTON1) {
+
+                    try {
+                        for (int i = 0; i < playingThreads.size(); i++) {
+                            playingThreads.get(i).stop();
+                        }
+
+                        System.out.println("stopped");
+                    } catch (Exception e1) {
+                        System.out.println("SHIT");
                     }
+                    SJButton.this.player = new Play();
+                    SJButton.this.player.setPlayingSong(song);
+                    SJButton.this.player.start();
+                    playingThreads.add(SJButton.this.player);
 
-                    System.out.println("stopped");
-                }catch (Exception e1)
-                {
-                    System.out.println("SHIT");
+
                 }
-                SJButton.this.player = new Play();
-                SJButton.this.player.setPlayingSong(song);
-                SJButton.this.player.start();
-                playingThreads.add(SJButton.this.player);
+                if (e.getButton() == MouseEvent.BUTTON3)
+                {
+                    popupMenu.show(SJButton.this, e.getX(), e.getY());
 
-
+                }
             }
         });
-
 
 
     }
--- src/Song.java
+++ src/Song.java
@@ -102,6 +102,5 @@ public class Song implements Serializable {
                 ", album='" + album + '\'' +
                 ", year='" + year + '\'' +
                 '}';
-
     }
 }
